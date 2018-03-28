8f8ebb7dbe9f2dd290e74572e456d9706a9ba462
winderbinder <junior_19000@hotmail.com>
2018-03-28 11:56:59 -0600

    changed seeds file to properly load post data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 User.create()
 
 10.times do |blog| 
-  Post.create!(title: "title#{blog}", body: "this is the populated body", date: "12/30/1995")
+  Post.create!(title: "title#{blog}", body: "this is the populated body", date: "Dec 30 1995")
 end
+
+puts "10 posts were created successfully"
