0e8d2c3054053baf57106dfb41f56eedd94a0098
winderbinder <junior_19000@hotmail.com>
2018-03-30 18:40:02 -0600

    added current user welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  devise_for :users
   resources :posts
 
   root to: 'posts#index'
