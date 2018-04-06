51d9bb543324350e38827de67a7b4ca3fc290672
winderbinder <junior_19000@hotmail.com>
2018-04-06 11:46:25 -0600

    code cleanup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   # access [:all, :user] => [:show, :index]
   
-  def index 
+  def index
     @posts = Post.all
   end
 
