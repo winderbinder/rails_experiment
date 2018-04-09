5570b98e21146cf81e63fd0c72d569d618cef73c
winderbinder <junior_19000@hotmail.com>
2018-04-09 17:24:50 -0600

    fixed routing issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index]
-  # access [:all, :user] => [:show, :index]
+  access all: [:show, :index], user: {except: [:destroy]}
   
   def index
     @posts = Post.all
